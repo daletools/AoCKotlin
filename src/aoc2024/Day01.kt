bc5fa74c922e1143388a68b4ae786af8f36c0231
Dale Hendricks <dalerhendricks@outlook.com>
2024-12-01 12:21:16 -0800

    Day1

--- src/aoc2024/Day01.kt
+++ src/aoc2024/Day01.kt
@@ -1,24 +1,55 @@
 package aoc2024
 import util.*
+import kotlin.math.abs
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var listA : MutableList<Int> = mutableListOf()
+        var listB : MutableList<Int> = mutableListOf()
+        var sum = 0
+        input.forEach {
+            var split = it.split(" ")
+            listA.add(split[0].toInt())
+            listB.add(split[3].toInt())
+        }
+
+        listA.sort()
+        listB.sort()
+
+        for (i in listA.indices) {
+            sum += abs(listA[i] - listB[i])
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var listA : MutableList<Int> = mutableListOf()
+        var listB : MutableList<Int> = mutableListOf()
+        var sum = 0
+        input.forEach {
+            var split = it.split(" ")
+            listA.add(split[0].toInt())
+            listB.add(split[3].toInt())
+        }
+
+        listA.sort()
+        listB.sort()
+
+        for (i in listA.indices) {
+            sum += listA[i] * listB.count { it == listA[i] }
+        }
+
+        return sum
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Input")
+    val start : Long = System.nanoTime()
     part1(input).println()
+    val mid : Long = System.nanoTime()
     part2(input).println()
+    val end : Long = System.nanoTime()
+
+    """Part 1 took ${(mid - start) / 1000000} ms""".println()
+    """Part 2 took ${(end - mid) / 1000000} ms""".println()
 }
