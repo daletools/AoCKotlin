1e46b9033de467ec7547816ad2c67b81b16dd46b
Dale Hendricks <dalerhendricks@outlook.com>
2025-12-05 15:49:32 -0800

    Day5 Part 2 on laptop

--- src/aoc2025/Day04.kt
+++ src/aoc2025/Day04.kt
@@ -3,7 +3,7 @@ package aoc2025
 import Day
 import util.ComplexInt
 
-private class Day04: Day(2025, 4) {
+private class Day04 : Day(2025, 4) {
   override fun partOne(input: List<String>): Long {
     val map = mutableListOf<ComplexInt>()
     for (row in input.indices) {
@@ -26,7 +26,30 @@ private class Day04: Day(2025, 4) {
     return sum.toLong()
   }
 
-  override fun partTwo(input: List<String>): Long {
+  override fun partTwo(input: List<String>): Long =
+    input.mapIndexed { row, line ->
+      line.mapIndexed { col, cell ->
+        if (cell != '@') {
+          false
+        } else {
+          listOf(
+            input.getOrNull(row - 1)?.getOrNull(col - 1) == '@',
+            input.getOrNull(row - 1)?.getOrNull(col) == '@',
+            input.getOrNull(row - 1)?.getOrNull(col + 1) == '@',
+            input.getOrNull(row)?.getOrNull(col - 1) == '@',
+            input.getOrNull(row)?.getOrNull(col + 1) == '@',
+            input.getOrNull(row + 1)?.getOrNull(col - 1) == '@',
+            input.getOrNull(row + 1)?.getOrNull(col) == '@',
+            input.getOrNull(row + 1)?.getOrNull(col + 1) == '@',
+          ).count { it } < 4
+        }
+      }
+    }
+      .flatten()
+      .count { it }
+      .toLong()
+
+  fun partTwotemp(input: List<String>): Long {
     val map = mutableListOf<ComplexInt>()
     for (row in input.indices) {
       for (col in input[row].indices) {
@@ -46,6 +69,7 @@ private class Day04: Day(2025, 4) {
         for (other in copy) {
           if (point == other) continue
           if (point.isDiagonallyAdjacent(other)) n++
+          if (n > 3) break
         }
         if (n < 4) {
           sum++
